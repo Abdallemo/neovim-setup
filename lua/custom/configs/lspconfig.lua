6ca7c7c7cbc0d412d49b1cd3b6d7f9785b7e307f
Abdallemo <learn3038it@gmail.com>
2025-05-06 02:36:21 +0800

    first init for neovim configurations

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,21 @@
+local base = require('plugins.configs.lspconfig')
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require('lspconfig')
+local servers = {
+  'tailwindcss',
+  'ts_ls',
+  'eslint',
+  'gopls',
+  -- 'bashls',
+  --'dockerls',
+  -- ' docker-compose-language-service'
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,44 @@
+local plugins = {
+  {
+    'nvimtools/none-ls.nvim',
+    --event= "VeryLazy",
+    opts = function()
+      return require('custom.configs.null-ls')
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      require('plugins.configs.lspconfig')
+      require('custom.configs.lspconfig')
+    end,
+  },
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = { -- set to setup table
+    },
+  },
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = {
+        'typescript-language-server',
+        'tailwindcss-language-server',
+        'eslint-lsp',
+        'prettierd',
+        'gopls',
+        'gofumpt',
+        'goimports-reviser',
+        -- 'dockerls',
+        --
+        -- 'bashls',
+
+
+      }
+    }
+  }
+}
+
+
+return plugins
