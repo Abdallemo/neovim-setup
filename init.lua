e53e6315b838caac6d1b26fcdee4a2c975382b29
Abdallemo <learn3038it@gmail.com>
2025-05-06 03:52:09 +0800

    enanced the side bar and some warnings

--- init.lua
+++ init.lua
@@ -1,3 +1,21 @@
+vim.deprecate = function() end
+
+local notify_original = vim.notify
+vim.notify = function(msg, ...)
+  if
+    msg
+    and (
+      msg:match 'position_encoding param is required'
+      or msg:match 'Defaulting to position encoding of the first client'
+      or msg:match 'multiple different client offset_encodings'
+    )
+  then
+    return
+  end
+  return notify_original(msg, ...)
+end
+
+
 require "core"
 
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
@@ -19,3 +37,14 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "✘",
+      [vim.diagnostic.severity.WARN] = "▲",
+      [vim.diagnostic.severity.INFO] = "»",
+      [vim.diagnostic.severity.HINT] = "⚑",
+    },
+  },
+})
+
